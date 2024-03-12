ef47956d31cd3458084eabef93012a9c560216eb
gg <gggiulio77@gmail.com>
2024-03-11 22:30:51 -0300

    feat: add app module

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { DbModule } from './db/db.module';
+
+@Module({
+  imports: [UsersModule, DbModule, ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
