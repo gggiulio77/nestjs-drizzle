8b1037795d71f722b900e972e1e1ae7416a68014
gg <gggiulio77@gmail.com>
2024-05-15 22:34:55 -0300

    feat: add user seed

---
+++ seeds/index.ts
@@ -0,0 +1,30 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import { CreateUserDto } from '@users/dto/create-user.dto';
+import * as argon2 from 'argon2';
+import { users } from '@db/schema';
+
+// TODO: replace relative paths with ts-paths @
+
+async function run() {
+    dotenv.config();
+
+    const sql = postgres(process.env.DB_URL, { max: 1 });
+    const db = drizzle(sql);
+
+    const { password, ...rest }: CreateUserDto = {
+        name: 'testing',
+        email: 'testing@testing.com',
+        password: 'testing',
+        role: 'admin',
+    };
+
+    const hashPassword = await argon2.hash(password);
+
+    await db.insert(users).values([{ ...rest, hashPassword }]);
+
+    await sql.end();
+}
+
+run();
